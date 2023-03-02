ce9f365a060d5e0d216720e736cc7d0b2853323a
Romeu Neves <51343870+rnevesphp@users.noreply.github.com>
2023-03-02 18:26:28 +0100

    creator class

--- controllers/views/creater.js
+++ controllers/views/creater.js
@@ -1,23 +1,82 @@
 class creator {
     constructor() {
-        this.doc = document
+        console.log('Hey creator');
     }
 
     destroyEl(element) {
         element.innerHTML = ""
-
         return element;
     }
 
     newElement(el) {
-        this.doc.createElement(el)
-
-        return el;
+        this.elem = this.doc.createElement(el)
+        return this.elem;
     }
 
-    addAttr(el, attr, value) {
-        el.setAttribute(attr, value);
+    addAttr(elem, attr, value) {
+        elem.setAttribute(attr, value);
+        return elem;
+    }
 
-        return el;
+    dashElem() {
+        this.doc = document;
+        this.navbar = this.doc.getElementById('nav-wrapper');
+
+        this.ul1 = this.newElement('ul')
+        this.addAttr(this.ul1, 'class', 'ul-dash hide-on-med-and-down')
+        this.addAttr(this.ul1, 'id', 'ul-dash')
+
+        // creando los elementos de una lista
+        this.li1 = this.newElement('li')
+        this.li2 = this.newElement('li')
+        this.li3 = this.newElement('li')
+        this.li4 = this.newElement('li')
+
+        // añadimos una clase a los elementos creados
+        this.addAttr(this.li1, 'class', 'dash-li')
+        this.addAttr(this.li2, 'class', 'dash-li')
+        this.addAttr(this.li3, 'class', 'dash-li')
+        this.addAttr(this.li4, 'class', 'dash-li')
+
+        // añadiendo contenido dentro de los elementos de las lista
+        this.li1.innerHTML = 'Dashboard';
+        this.li2.innerHTML = 'Wallets';
+        this.li3.innerHTML = 'Settings';
+        this.li4.innerHTML = 'Profile';
+
+        // añadiendo los clicks
+        // this.li1.addEventListener('click', this.initialPage.bind(this))
+        this.li2.addEventListener('click', this.wallets.bind(this))
+        this.li3.addEventListener('click', this.settings.bind(this))
+        this.li4.addEventListener('click', this.profile.bind(this))
+
+        // añadiendo los elementos creados a la lista desordenada
+        this.ul1.appendChild(this.li1);
+        this.ul1.appendChild(this.li2);
+        this.ul1.appendChild(this.li3);
+        this.ul1.appendChild(this.li4);
+
+        // añadiendo la lista dentro del navbar 
+        this.navbar.appendChild(this.ul1);
+
+        // Haciendo el remove del logo para que este cuadrante entre la lista desordenada
+        this.logo = this.doc.querySelector('.brand-logo');
+        this.destroyEl(this.logo)
+
+        this.iP = new initialPage()
+        this.iP.createLogOut();
+    }
+    wallets() {
+        this.w = new wallets();
+    }
+    struc() {
+        this.st = new navStruc();
+    }
+
+    profile() {
+        this.pf = new profile();
+    }
+    settings() {
+        this.config = new config();
     }
 }
--- controllers/views/dashboard.js
+++ controllers/views/dashboard.js
@@ -2,38 +2,32 @@ class initialPage {
   constructor() {
     console.log("AQUI LLEGA... welcome... dashboard..");
 
-    this.a = document.getElementById('logOutHref')
-    this.erase(this.a)
+    this.ct = new creator();
 
     this.mainCode = document.getElementById('main-code')
-    this.deleteMaincode(this.mainCode)
-
+    this.a = document.getElementById('logOutHref')
     this.btnMobile = document.getElementById('btnMobile')
-    this.btnMobile.addEventListener('click', this.logout.bind(this));
-
     this.iconLogOut = document.getElementById('icon-btnMobile');
+
+    this.btnMobile.addEventListener('click', this.logout.bind(this));
     this.iconLogOut.className = "fa-solid fa-xmark";
 
-    this.dashElem();
-    this.struc();
+    this.ct.destroyEl(this.a)
+    this.ct.destroyEl(this.mainCode)
+
+    this.ct.dashElem();
   }
-
-
 
   /**
    * this method create a button to logout from the user account
    */
   createLogOut() {
-    this.logOutBTN = document.createElement('i');
-    this.logOutBTN.setAttribute('class', 'icon-logout fa-solid fa-xmark')
-
+    this.logOutBTN = this.ct.newElement('i');
+    this.ct.addAttr(this.logOutBTN, 'class', 'icon-logout fa-solid fa-xmark')
     this.li = document.getElementById('logoutbtn')
     this.li.appendChild(this.logOutBTN)
-
     this.logOutBTN.addEventListener("click", this.logout.bind(this));
   }
-
-
 
   /**
    * This method contains the logout logical
@@ -44,110 +38,5 @@ class initialPage {
 
     this.session.clear()
     location.reload();
-  }
-
-  deleteMaincode(elem) {
-    elem.innerHTML = "";
-
-    return elem;
-  }
-
-  erase(elem) {
-    this.createLogOut();
-    elem.innerHTML = "";
-
-    return elem;
-  }
-
-
-
-  dashElem() {
-    this.doc = document;
-
-    this.navbar = this.doc.getElementById('nav-wrapper');
-
-    this.ul1 = this.doc.createElement('ul')
-    this.ul1.setAttribute('class', 'ul-dash hide-on-med-and-down')
-    this.ul1.setAttribute('id', 'ul-dash')
-
-    // icon 1
-    this.i1 = this.doc.createElement('i')
-    this.i1.setAttribute('class', 'fa-solid fa-table-columns')
-    this.i1.setAttribute('id', 'nav-mobile-2-icon')
-    // icon 2 
-    this.i2 = this.doc.createElement('i')
-    this.i2.setAttribute('class', 'fa-solid fa-wallet')
-    this.i2.setAttribute('id', 'nav-mobile-2-icon')
-    // icon 3
-    this.i3 = this.doc.createElement('i')
-    this.i3.setAttribute('class', 'fa-solid fa-gear')
-    this.i3.setAttribute('id', 'nav-mobile-2-icon')
-    // icon 4
-    this.i4 = this.doc.createElement('i')
-    this.i4.setAttribute('class', 'fa-solid fa-user')
-    this.i4.setAttribute('id', 'nav-mobile-2-icon')
-
-    // creando los elementos de una lista
-    this.li1 = this.doc.createElement('li')
-    this.li2 = this.doc.createElement('li')
-    this.li3 = this.doc.createElement('li')
-    this.li4 = this.doc.createElement('li')
-
-    // añadimos una clase a los elementos creados
-    this.li1.setAttribute('class', 'dash-li')
-    this.li2.setAttribute('class', 'dash-li')
-    this.li3.setAttribute('class', 'dash-li')
-    this.li4.setAttribute('class', 'dash-li')
-
-    // añadiendo contenido dentro de los elementos de las lista
-    this.li1.innerHTML = 'Dashboard';
-    this.li2.innerHTML = 'Wallets';
-    this.li3.innerHTML = 'Settings';
-    this.li4.innerHTML = 'Profile';
-
-    // añadiendo los clicks
-    this.li1.addEventListener('click', this.initPage.bind(this))
-    this.li2.addEventListener('click', this.wallets.bind(this))
-    this.li3.addEventListener('click', this.settings.bind(this))
-    this.li4.addEventListener('click', this.profile.bind(this))
-
-    // añadiendo los iconos como hijos de los elementos
-    this.li1.appendChild(this.i1)
-    this.li2.appendChild(this.i2)
-    this.li3.appendChild(this.i3)
-    this.li4.appendChild(this.i4)
-
-    // añadiendo los elementos creados a la lista desordenada
-    this.ul1.appendChild(this.li1);
-    this.ul1.appendChild(this.li2);
-    this.ul1.appendChild(this.li3);
-    this.ul1.appendChild(this.li4);
-
-    // añadiendo la lista dentro del navbar 
-    this.navbar.appendChild(this.ul1);
-
-    // Haciendo el remove del logo para que este cuadrante entre la lista desordenada
-    this.logo = this.doc.querySelector('.brand-logo');
-    this.logo.remove()
-  }
-
-  settings() {
-    this.config = new config();
-  }
-
-  profile() {
-    this.pf = new profile();
-  }
-
-  initPage() {
-
-  }
-
-  wallets() {
-    this.w = new wallets();
-  }
-
-  struc() {
-    this.st = new navStruc();
   }
 }
